8623c02c90152546619c0b95ebcb9c1707ff99a5
Alex Sexton <AlexSexton@gmail.com>
2011-08-15 23:57:37 -0500

    Don't let the simple server leak out of a ctrl-c

--- static/serveDev.sh
+++ static/serveDev.sh
@@ -1,7 +1,7 @@
 sudo gem install bundler --no-rdoc --no-ri
 bundle install --local
 cd dev
+sudo python -m SimpleHTTPServer 80 &
 compass watch . &
 echo "Starting up simple static server..." &
-sudo python -m SimpleHTTPServer 80 &
 wait
